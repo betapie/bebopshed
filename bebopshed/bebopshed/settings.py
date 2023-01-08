056811543995219b5b4d8bdcf7000d83f0f76834
betapie <ma.lauterbach@gmail.com>
2023-01-08 19:33:36 +0100

    decouple environment

--- bebopshed/bebopshed/settings.py
+++ bebopshed/bebopshed/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import decouple
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d!cx*5&e3zq^t)h$u*eqvh3^$)f)de%%v$nq-^35kp=05f&nsq'
+SECRET_KEY = decouple.config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
