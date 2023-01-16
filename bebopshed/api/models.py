e4e56646cc76a28f21bcfc9fd1b811fcfbdb4071
betapie <ma.lauterbach@gmail.com>
2023-01-16 21:26:50 +0100

    defined models

--- bebopshed/api/models.py
+++ bebopshed/api/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+class Instrument(models.Model):
+    name: models.CharField(max_length=64)
+
+
+class Artist(models.Model):
+    name: models.CharField(max_length=64)
+    instrument: models.ForeignKey(
+        Instrument, models.SET_NULL, blank=True, null=True)
+
+
+class Progression(models.Model):
+    sequence: models.CharField(max_length=32)
+    common_name: models.CharField(max_length=64)
+
+
+class Album(models.Model):
+    artist: models.ForeignKey(Artist, models.SET_NULL, blank=True, null=True)
+    name: models.CharField(max_length=64)
+
+
+class Line(models.Model):
+    # using lilypond nomenclature
+    class Key(models.TextChoices):
+        C = "C", "c"
+        C_sharp = "CIS", "cis"
+        D_flat = "DES", "des"
+        D = "D", "d"
+        D_sharp = "DIS", "dis"
+        E_flat = "ES", "es"
+        E = "E", "e"
+        F = "F", "f"
+        F_sharp = "FIS", "fis"
+        G_flat = "GES", "ges"
+        G = "G", "g"
+        G_sharp = "GIS", "gis"
+        A_flat = "AS", "as"
+        A = "A", "a"
+        B_flat = "BES", "bes"
+        B = "B", "b"
+
+    line: models.TextField()
+    chords: models.TextField()
+    progression: models.ForeignKey(
+        Progression, models.SET_NULL, blank=True, null=True)
+    key: models.CharField(max_length=3, choices=Key.choices)
+    artist: models.ForeignKey(Artist, models.SET_NULL, blank=True, null=True)
+    album: models.ForeignKey(Album, models.SET_NULL, blank=True, null=True)
+    year: models.IntegerField(blank=True, null=True)
