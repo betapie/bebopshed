2ceac5fda252e50c8efcaf41c1241fc9d6302ce1
betapie <ma.lauterbach@gmail.com>
2023-01-21 14:16:48 +0100

    flake8 linting

--- bebopshed/api/views.py
+++ bebopshed/api/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+
 def main(request):
     return HttpResponse('Welcome to bebopshed API')
+
 
 @api_view(['GET'])
 def generate_line(request):
