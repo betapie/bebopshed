051460d8a68e79357e6fc0ccdd68f2c69fbfd39f
betapie <ma.lauterbach@gmail.com>
2023-01-22 11:23:48 +0100

    renamed module

--- bebopshed/api/views.py
+++ bebopshed/api/views.py
@@ -6,23 +6,11 @@ from rest_framework.decorators import api_view
 
 from .models import Line
 
-from music_renderer.music_renderer import MusicRenderer
+from lily_proc.music_renderer import MusicRenderer
 
 
 def main(request):
     return HttpResponse('Welcome to bebopshed API')
-
-
-@api_view(['GET'])
-def generate_line_fallback(request):
-    result = {}
-
-    with open("music_renderer/examples/remember1.svg", 'r') as svg_file:
-        content = svg_file.read()
-        result["line"] = content
-        result["artist"] = "Wynton Kelly"
-        result["song"] = "Remember"
-    return Response(result)
 
 
 @api_view(['GET'])
