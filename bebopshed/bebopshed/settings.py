0a29f4dd4ca158205a9d6cca36fd0bfbfd643edb
betapie <ma.lauterbach@gmail.com>
2023-03-19 13:31:13 +0100

    migration to postgres

--- bebopshed/bebopshed/settings.py
+++ bebopshed/bebopshed/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'bebopshed.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': decouple.config("DB_NAME"),
+        'USER': decouple.config("DB_USER"),
+        'PASSWORD': decouple.config("DB_PASSWORD"),
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
