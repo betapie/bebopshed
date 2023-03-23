edd6c8705475011155530d4a1b79758b87c6be4c
betapie <ma.lauterbach@gmail.com>
2023-03-23 22:03:26 +0100

    batch sanitizing db

---
+++ bebopshed/api/management/commands/sanitize_lines.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from api.models import Line
+from lily_proc.line_parser import LineParser
+
+
+class Command(BaseCommand):
+    help = "TODO"
+
+    def handle(self, *args, **kwargs):
+        parser = LineParser()
+        for line in Line.objects.all():
+            print(f"Sanitized {line}")
+            line.line = parser.sanitize(line.line)
+            line.save()
